da6a6cea86b4f2cf203b3f855c0b06f095c23055
Martin Lubo <martinlubo588@gmail.com>
2021-02-16 01:22:49 -0300

    Web funcionando OK

--- build/routes/web/productos/productos.js
+++ build/routes/web/productos/productos.js
@@ -19,6 +19,20 @@ router.get('/alta', (req, res) => __awaiter(void 0, void 0, void 0, function* ()
     let products = yield productCRUD_1.listProducts();
     res.render("pages/index", { info: products });
 }));
+router.post('/alta', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    if ((!req.body.title || req.body.title === "" || req.body.price === null || req.body.price === undefined || !req.body.thumbnail || req.body.thumbnail === "")) {
+        res.status(400).send('Los parametros enviados son incorrectos');
+    }
+    else {
+        yield productCRUD_1.saveProduct(req.body.title, parseInt(req.body.price), req.body.thumbnail);
+        res.redirect('/productos/alta');
+        try {
+        }
+        catch (error) {
+            res.status(500).send('Error de la aplicacion' + error);
+        }
+    }
+}));
 router.get('/vista', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     let products = yield productCRUD_1.listProducts();
     res.render("pages/indexListado", { info: products });
--- src/routes/web/productos/productos.ts
+++ src/routes/web/productos/productos.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import {listProducts} from '../../../archivos/productCRUD';
+import {listProducts, saveProduct} from '../../../archivos/productCRUD';
 
 let router = express.Router();
 
@@ -7,6 +7,18 @@ let router = express.Router();
 router.get('/alta', async (req, res) => {
     let products = await listProducts();
     res.render("pages/index", {info: products});
+});
+router.post('/alta', async (req, res) => {
+    if((!req.body.title || req.body.title === "" || req.body.price === null || req.body.price === undefined || !req.body.thumbnail || req.body.thumbnail === "") ){
+        res.status(400).send('Los parametros enviados son incorrectos');
+    } else {
+        await saveProduct(req.body.title, parseInt(req.body.price), req.body.thumbnail);
+        res.redirect('/productos/alta')
+        try {
+        } catch (error) { 
+        res.status(500).send('Error de la aplicacion' + error);
+        }
+    }
 });
 
 router.get('/vista', async (req, res) => {
