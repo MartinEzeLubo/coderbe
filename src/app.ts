d5849fcab1cd08b8abbdb71a9900d7a45adf97c3
Martin Lubo <martinlubo588@gmail.com>
2021-02-16 14:18:55 -0300

    intentando encontrarle la vuelta al socketio

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,28 @@
 import express from 'express';
+import { io } from 'socket.io-client';
 import apiRouter from './routes/api/api';
 import webRouter from './routes/web/web';
 
 const port = 8080;
 const app = express();
 const http = require('http').Server(app);
-const socket = require('socket.io')(http);
+export const socket = require('socket.io')(http);
 
 
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.use(express.urlencoded({extended: true}));
-app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
 app.use('/api', apiRouter);
 app.use('/', webRouter);
 
 
+socket.on('new-product', (data) =>{
+  console.log(data);
 
+})
 
 http.listen(port, () => {
   
