51dd856585543bd0b70a7395106f7282e297277b
Martin Lubo <martinlubo588@gmail.com>
2021-06-20 16:39:54 -0300

    supertest

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,7 @@
 const assert = require('assert').strict
 const axios = require('axios')
+const request = require('supertest')('http://localhost:8080')
+const expect = require('chai').expect
 
 
 const url = "http://localhost:8080/productos"
@@ -7,7 +9,7 @@ let idAEliminar;
 
 describe("testeo de la api", function(){
     
-    it('deberia existir productos',()=>{
+    it('deberia existir productos', async ()=>{
             axios.get(url)
             .then(res=>{
                 assert.notStrictEqual(res.data.data.products.length, 0)
@@ -57,8 +59,55 @@ describe("testeo de la api", function(){
     })
 })
 
+describe("SUPERTEST - testeo de la api con supertest", function(){
     
-    // return id = await axios.post(url, data)
-    // .then(res=>{
-    //     return res.data.data.createProduct.id
-    // })
+    it('SUPERTEST - Status 200',async ()=>{
+            let response = await request.get('/productos')
+            expect(response.status).to.eql(200)
+            
+    })
+    it('SUPERTEST - crear nuevo producto', async ()=>{
+        const data = {
+            "nombre": "Producto que se va a eliminar",
+            "descripcion": "descripcion vieja",
+            "precio": 35999,
+            "codigo": "10220001",
+            "stock": 10,
+            "thumbnail": "https://garmin.com.ar/Image/0/700_700-010-01689-00_1.jpg"
+        }
+        idAEliminar = await request.post('/productos').send(data)
+        .then(res=>{
+            expect(res.body.data.createProduct.id).to.not.eql(undefined)
+            return res.body.data.createProduct.id
+        })        
+    })
+    it('SUPERTEST - Actualizar descripcion del producto', async ()=>{
+        const data = {
+            "id": `${idAEliminar}`,
+            "nombre": "Producto que se va a eliminar",
+            "descripcion": "descripcion nueva",
+            "precio": 35999,
+            "codigo": "10220001",
+            "stock": 10,
+            "foto": "https://garmin.com.ar/Image/0/700_700-010-01689-00_1.jpg"
+        }
+        await request.put('/productos').send(data)
+        .then(res=>{
+            expect(res.body.descripcion).to.eql("descripcion nueva")
+            
+        })        
+    })
+    it('SUPERTEST - Encontrar el producto creado en el test anterior',async ()=>{
+        await request.get(`/productos/?id=${idAEliminar}`)
+        .then(res=>{
+            expect(res.body.data.product.id).to.eql(idAEliminar)
+        })
+    })    
+    it('Eliminar el producto creado',async ()=>{
+        await request.delete(`/productos/${idAEliminar}`)
+        .then(res=>{
+            expect(res.body._id).to.eql(idAEliminar)
+        })
+    })
+
+})
